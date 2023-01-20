b0dd9f4f81576ab2350facc256da58001d1ee0e6
Rohit <rohitparmar@gmail.com>
2023-01-20 23:42:38 +0530

    Integrated Redux old version to understand difference between both.

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,43 +1,56 @@
-import React, { Fragment } from 'react';
-import {CgMouse} from "react-icons/cg";
+import React, { Fragment, useEffect } from 'react';
+
+//This is ScrollMouse Icon from react-icons package
+import { CgMouse } from "react-icons/cg";
+
+//This is Product Card
 import Product from "./Product.js"
 import "./Home.css"
 
-const product = {
-    name: "Blue Tshirt",
-    images: [{url: "https://www.bing.com/th/id/OIP.pe-Sjx5Bu-_z62RULHFKgwHaJ4?w=146&h=195&c=7&r=0&o=5&dpr=1.5&pid=1.7"}],
-    price: "₹3000",
-    _id: "rohitparmar",
-};
+//To set Title of webpage using this metadata
+import MetaData from '../layout/MetaData.js';
+
+import { getProduct } from "../../actions/productAction"
+import { useSelector, useDispatch } from "react-redux"
+import Loader from '../layout/Loader/Loader.js';
+
+
 const Home = () => {
-  return (
-    <Fragment>
-        <div className="banner">
-            <p>Welcome to Ecommerce</p>
-            <h1>FIND AMAZING PRODUCTS BELOW</h1>
 
-            <a href="#container">
-                <button>Scroll <CgMouse/></button>
-            </a>
-        </div>
+    const dispatch = useDispatch();
+    const { loading, error, products, productsCount } = useSelector(state => state.products)
+    useEffect(() => {
+        dispatch(getProduct());
+    }, [dispatch])
 
-        <h2 className='homeHeading'>Featured Products</h2>
-        <div className="container" id="container">
-            <Product product={product} />
-            <Product product={product} />
-            <Product product={product} />
-            <Product product={product} />
-            <Product product={product} />
-            <Product product={product} />
-            <Product product={product} />
-            <Product product={product} />
-            <Product product={product} />
-            <Product product={product} />
-            <Product product={product} />
-            <Product product={product} />
-        </div>
-    </Fragment>
-  )
+    return (
+        <Fragment>
+            {
+                loading ? <Loader/>:
+                <Fragment>
+            <MetaData title="Ecommerce" />
+            <div className="banner">
+                <p>Welcome to Ecommerce</p>
+                <h1>FIND AMAZING PRODUCTS BELOW</h1>
+
+                <a href="#container">
+                    <button>Scroll <CgMouse /></button>
+                </a>
+            </div>
+            {
+                console.log(products)
+            }
+            <h2 className='homeHeading'>Featured Products</h2>
+            <div className="container" id="container">
+                {products &&
+                    products.map((product) => (
+                        <Product key={product._id} product={product} />
+                    ))}
+            </div>
+        </Fragment>
+            }
+        </Fragment>
+    )
 }
 
 export default Home
